e6c859239846b93e44ff38095bf412b1ed150928
DESKTOP-92V0EM9\po <jianghaobao123@gmail.com>
2020-10-03 17:25:15 +0800

    added dl file method

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/haobaojiang/golib
 go 1.11
 
 require github.com/gogf/gf v1.11.7
+
+require github.com/cavaliercoder/grab v2.0.0+incompatible
--- golib.go
+++ golib.go
@@ -1,7 +1,46 @@
 package golib
 
-import "regexp"
+import (
+	"regexp"
+	"time"
+
+	"github.com/cavaliercoder/grab"
+	"github.com/gogf/gf/errors/gerror"
+)
 
 func IsDigOrAlpha(s string) bool {
 	return regexp.MustCompile(`^[A-Za-z0-9]+$`).MatchString(s)
+}
+
+func DlFileWithProgress(url string, dlDst string, progressCallBack func(dlProgress int32, dlSpeed float64)) error {
+
+	client := grab.NewClient()
+	req, err := grab.NewRequest(dlDst, url)
+	if err != nil {
+		return gerror.Wrap(err, "failed to create request")
+	}
+
+	req.NoResume = true
+	resp := client.Do(req)
+	if err != nil {
+		return gerror.Wrap(err, "failed to do the request")
+	}
+
+	t := time.NewTicker(1 * time.Second)
+	defer t.Stop()
+
+Loop:
+	for {
+		select {
+		case <-t.C:
+			progressCallBack(int32(100*resp.Progress()), resp.BytesPerSecond())
+		case <-resp.Done:
+			if resp.Err() != nil {
+				return gerror.Wrap(err, "failed to download")
+			}
+			progressCallBack(100, resp.BytesPerSecond())
+			break Loop
+		}
+	}
+	return nil
 }
